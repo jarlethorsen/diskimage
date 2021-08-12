79b2dcd22c67725bf785704b2010a73225dbce48
Jarle Thorsen <jarlethorsen@gmail.com>
2021-08-12 16:52:25 +0200

    Clean up code to remove duplicate usage of "diskimage"

--- diskimage/__main__.py
+++ diskimage/__main__.py
@@ -3,23 +3,22 @@ import argparse
 import os
 import sys
 import diskimage
-import diskimage.diskimage as di
 
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
-        usage="%(prog)s <command> <options> disk-image-file",
+        usage="%(prog)s <command> <options> diskimagefile",
         description="Get information from disk-images."
     )
 
     parser.add_argument('-v', '--verbose', help='Add verbosity, -vv to enable debugging', action='count', default=0)
     parser.add_argument('--version', help='Print version information')
-    parser.add_argument('disk_image', nargs='?')
+    parser.add_argument('diskimagefile', nargs='?')
     return parser
 
 
-def print_info(diskimage):
-    image = di.DiskImage.from_file(diskimage)
+def print_info(diskimage_file):
+    image = diskimage.DiskImage.from_file(diskimage_file)
     if image:
         if image.filesystems:
             print('[*] Filesystems found:')
@@ -28,22 +27,22 @@ def print_info(diskimage):
         else:
             print(f'Found no supported filesystems in this diskimage.')
     else:
-        print(f'*ERROR* File {diskimage} is not a supported diskimage')
+        print(f'*ERROR* File {diskimage_file} is not a supported diskimage')
 
 
 def main():
     parser = init_argparse()
     args = parser.parse_args()
-    disk_image = args.disk_image
-    if disk_image is None:
+    diskimage_file = args.diskimagefile
+    if diskimage_file is None:
         print(f'diskimage v{diskimage.__version__}')
         parser.print_help()
         sys.exit(0)
 
-    if os.path.isfile(diskimage):
-        print_info(diskimage)
+    if os.path.isfile(diskimage_file):
+        print_info(diskimage_file)
     else:
-        print(f'ERROR! diskimage {diskimage} does not exist!\nPlease provide a diskimage as input')
+        print(f'ERROR! diskimage {diskimage_file} does not exist!\nPlease provide a diskimage as input')
         parser.print_help()
 
 
